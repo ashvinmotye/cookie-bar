34a688fb996be097d1b1207b570f8db02ad43ea3
Ashvin Motye <22303668+ashvinmotye@users.noreply.github.com>
2020-06-13 22:13:32 +0400

    Compile JS + modify folder structure

---
+++ assets/js/custom.js
@@ -0,0 +1,2 @@
+console.log("init");
+//# sourceMappingURL=custom.js.map
--- gulpfile.js
+++ gulpfile.js
@@ -2,24 +2,43 @@ const gulp = require("gulp"),
   sass = require("gulp-sass"),
   autoprefixer = require("gulp-autoprefixer"),
   sourcemaps = require("gulp-sourcemaps"),
-  cleanCSS = require("gulp-clean-css");
+  cleanCSS = require("gulp-clean-css"),
+  uglify = require("gulp-uglify"),
+  concat = require("gulp-concat");
 
-const path = `./assets`;
+const source = `./src`;
+const output = `./assets`;
 
 const styles = (cb) => {
   gulp
-    .src(`${path}/scss/*scss`)
+    .src(`${source}/scss/*scss`)
     .pipe(sourcemaps.init())
     .pipe(sass())
     .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
     .pipe(autoprefixer())
     .pipe(cleanCSS({ compatibility: "ie8" }))
-    .pipe(gulp.dest(`${path}/css`))
+    .pipe(gulp.dest(`${output}/css`))
     .pipe(sourcemaps.write(`./`))
-    .pipe(gulp.dest(`${path}/css`));
+    .pipe(gulp.dest(`${output}/css`));
+
+  cb();
+};
+
+const scripts = (cb) => {
+  const fileName = `custom.js`;
+
+  gulp
+    .src(`${source}/js/**/*.js`)
+    .pipe(sourcemaps.init())
+    .pipe(concat(fileName))
+    .pipe(uglify())
+    .pipe(gulp.dest(`${output}/js`))
+    .pipe(sourcemaps.write(`./`))
+    .pipe(gulp.dest(`${output}/js`));
 
   cb();
 };
 
 exports.styles = styles;
-exports.default = styles;
+exports.scripts = scripts;
+exports.default = gulp.series([styles, scripts]);
